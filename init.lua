444f6d4990c553be2e6cd411fa00e013cf1bfa7b
joshua hammer <jph33@outlook.com>
2025-07-22 11:29:51 -0400

    surely search crafting works now

--- init.lua
+++ init.lua
@@ -13,13 +13,12 @@ local config = {
 		remaps = {
 			["m5"] = "f3",
 			["capslock"] = "0",
-			["f3"] = "f20",
-			["0"] = "f21",
 
 			["LeftShift"] = "LeftCtrl",
 			["LeftCtrl"] = "LeftShift",
 			["D"] = "Backspace",
 			["Grave"] = "Tab",
+			["Tab"] = "Dot",
 		},
 	},
 	theme = {
